0c66a919005ddb4c246fa0f5419dc4680e4c66cb
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-10-07 20:21:48 +0900

    Initial commit.

---
+++ Assets/AudioSpectrum.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSpectrum : MonoBehaviour
+{
+    [SerializeField]
+    int numberOfSamples = 1024;
+
+    [SerializeField]
+    int numberOfBands = 8;
+
+    float[] rawSpectrum;
+    float[] bandLevels;
+
+    public float[] BandLevels {
+        get {
+            return bandLevels;
+        }
+    }
+
+    public int NumberOfSamples {
+        get {
+            return numberOfSamples;
+        }
+        set {
+            numberOfSamples = value;
+            CheckBufferUpdate();
+        }
+    }
+
+    public int NumberOfBands {
+        get {
+            return numberOfBands;
+        }
+        set {
+            numberOfBands = value;
+            CheckBufferUpdate();
+        }
+    }
+
+    void CheckBufferUpdate()
+    {
+        if (rawSpectrum != null && numberOfSamples != rawSpectrum.Length) {
+            rawSpectrum = new float[numberOfSamples];
+        }
+        if (bandLevels != null && numberOfBands != bandLevels.Length) {
+            bandLevels = new float[numberOfBands];
+        }
+    }
+
+    void Awake ()
+    {
+        rawSpectrum = new float[numberOfSamples];
+        bandLevels = new float[numberOfBands];
+    }
+    
+    void Update ()
+    {
+        AudioListener.GetSpectrumData (rawSpectrum, 0, FFTWindow.BlackmanHarris);
+        
+        var coeff = Mathf.Log (rawSpectrum.Length);
+        var offs = 0;
+        for (var i = 0; i < bandLevels.Length; i++) {
+            var next = Mathf.Exp (coeff / bandLevels.Length * (i + 1));
+            var weight = 1.0f / (next - offs);
+            var sum = 0.0f;
+            for (; offs < next; offs++) {
+                sum += rawSpectrum [offs];
+            }
+            bandLevels [i] = Mathf.Sqrt (weight * sum);
+        }
+    }
+}
---
+++ Assets/Editor/AudioSpectrumInspector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioSpectrum))]
+public class AudioSpectrumInspector : Editor
+{
+    static string[] sampleOptionStrings = {
+        "64", "128", "256", "512", "1024", "2048", "4096"
+    };
+    static int[] sampleOptions = {
+        64, 128, 256, 512, 1024, 2048, 4096
+    };
+    AnimationCurve curve;
+
+    void UpdateCurve ()
+    {
+        curve = new AnimationCurve ();
+
+        var spectrum = target as AudioSpectrum;
+        var bands = spectrum.BandLevels;
+        var time = 0.0f;
+        foreach (var band in bands) {
+            curve.AddKey (time, band);
+            time += 1.0f / bands.Length;
+        }
+    }
+
+    public override void OnInspectorGUI ()
+    {
+        var spectrum = target as AudioSpectrum;
+
+        if (EditorApplication.isPlaying) {
+            UpdateCurve ();
+        } else if (curve == null) {
+            curve = new AnimationCurve ();
+        }
+
+        spectrum.NumberOfBands = EditorGUILayout.IntField ("Number of Bands", spectrum.NumberOfBands);
+        spectrum.NumberOfSamples = EditorGUILayout.IntPopup ("Number of Samples", spectrum.NumberOfSamples, sampleOptionStrings, sampleOptions);
+
+        EditorGUILayout.CurveField (curve, Color.white, new Rect (0, 0, 1.0f, 0.5f), GUILayout.Height (64));
+
+        if (EditorApplication.isPlaying) {
+            EditorUtility.SetDirty (target);
+        }
+    }
+}
