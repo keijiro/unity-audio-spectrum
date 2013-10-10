85e08b0a6cd1d77e26c0502130d12f872746fb3f
Keijiro Takahashi <keijiro@MSX.local>
2013-10-10 14:16:39 +0900

    Added the oblique projection tools.

---
+++ Assets/Editor/ObliqueProjectionEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ObliqueProjection))]
+public class ObliqueProjectionEditor : Editor
+{
+    public override void OnInspectorGUI ()
+    {
+        var t = target as ObliqueProjection;
+
+        GUI.changed = false;
+        t.angle = EditorGUILayout.Slider ("Angle", t.angle, -180.0f, 180.0f);
+        t.zScale = EditorGUILayout.Slider ("Z Scale", t.zScale, 0.0f, 1.0f);
+        t.zOffset = EditorGUILayout.FloatField ("Z Offset", t.zOffset);
+
+        if (GUI.changed) {
+            EditorUtility.SetDirty (target);
+            t.Apply ();
+        }
+    }
+}
---
+++ Assets/ObliqueProjection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode, RequireComponent(typeof(Camera))]
+public class ObliqueProjection : MonoBehaviour
+{
+    public float angle = 45.0f;
+    public float zScale = 0.5f;
+    public float zOffset = 0.0f;
+
+    public void Apply ()
+    {
+        camera.orthographic = true;
+        var orthoHeight = camera.orthographicSize;
+        var orthoWidth = camera.aspect * orthoHeight;
+        var m = Matrix4x4.Ortho (-orthoWidth, orthoWidth, -orthoHeight, orthoHeight, camera.nearClipPlane, camera.farClipPlane);
+        var s = zScale / orthoHeight;
+        m [0, 2] = +s * Mathf.Sin (Mathf.Deg2Rad * -angle);
+        m [1, 2] = -s * Mathf.Cos (Mathf.Deg2Rad * -angle);
+        m [0, 3] = -zOffset * m [0, 2];
+        m [1, 3] = -zOffset * m [1, 2];
+        camera.projectionMatrix = m;
+    }
+
+    void OnEnable ()
+    {
+        Apply ();
+    }
+
+    void OnDisable ()
+    {
+        camera.ResetProjectionMatrix ();
+    }
+}
